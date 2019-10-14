25bc246ac1f4ccac659581d47a0e39d34da02900
OliveiraLuis <oliveira1eduardo2luis3@gmail.com>
2019-10-14 18:39:56 -0300

    Initial commit

---
+++ routes/aviao.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Gerenciar Aviões', subtitle: 'Cadastre e consulte aviões' });
+});
+
+router.get('/cadastrar', function(req, res) {
+    res.render('index', { title: 'Cadastrar Aviões', subtitle: 'Adicionar um novo avião para nossa base' });
+});
+
+router.get('/consultar', function(req, res) {
+    res.render('index', { title: 'Listar Aviões', subtitle: 'Verificar aviões cadastrados e tratar suas informações' });
+});
+
+router.post('/cadastrar', function(req, res) {
+    res.send('Not implemented yet');
+});
+
+router.put('/editar', function(req, res) {
+    res.send('Not implemented yet');
+});
+
+router.delete('/excluir', function(req, res) {
+    res.send('Not implemented yet');
+});
+module.exports = router;
