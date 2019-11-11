dc304757edfe9bb926e2415d5564d9c973105225
OliveiraLuis <oliveira1eduardo2luis3@gmail.com>
2019-11-11 16:26:36 -0300

    cadastrar e listar avião

--- routes/aviao.js
+++ routes/aviao.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const Aviao = require('../models/aviao')
+const Crud = require('../models/Crud')
 
 router.get('/', function(req, res) {
   res.render('aviao/index', { title: 'Gerenciar Aviões', subtitle: 'Cadastre e consulte aviões' });
@@ -11,13 +11,13 @@ router.get('/cadastrar', function(req, res) {
 });
 
 router.get('/consultar', async (req, res) => {
-    const aviao = new Aviao()
+    const aviao = new Crud('aviao')
     
     res.render('aviao/listar', { title: 'Listar Aviões', subtitle: 'Verificar aviões cadastrados e tratar suas informações', avioes: await aviao.find() });
 });
 
 router.post('/cadastrar', function(req, res) {
-    const aviao = new Aviao()
+    const aviao = new Crud('aviao')
 
     aviao.insert(req.body)
     res.render('aviao/cadastrar', { title: 'Cadastrar Aviões', subtitle: 'Avião cadastrado com sucesso' });
@@ -27,7 +27,10 @@ router.put('/editar', function(req, res) {
     res.send('Not implemented yet');
 });
 
-router.delete('/excluir', function(req, res) {
-    res.send('Not implemented yet');
+router.delete('/excluir/:registro?', async (req, res) => {
+    const aviao = new Crud('aviao')
+
+    aviao.remove({registro: req.params.registro})
+    res.render('aviao/listar', { title: 'Listar Aviões', subtitle: 'Avião removido com sucesso', avioes: await aviao.find() });
 });
 module.exports = router;
